174819c0abdb5800ce440a53b1612d6e8eb0c8cb
Wissal65 <wissalbara65@gmail.com>
2024-06-23 00:26:12 +0100

    Add assets: Constants.js, Images.js, and images

---
+++ assets/Constants.js
@@ -0,0 +1,12 @@
+import { Dimensions } from 'react-native';
+
+const Constants = {
+    MAX_WIDTH: Dimensions.get('window').width,
+    MAX_HEIGHT: Dimensions.get('window').height,
+    BIRD_WIDTH: 50,
+    BIRD_HEIGHT: 34,
+    PIPE_WIDTH: 100,
+    GAP_SIZE: 330,
+};
+
+export default Constants;
---
+++ assets/Images.js
@@ -0,0 +1,20 @@
+export default Images = {
+    background: require('./images/background.png'),
+    floor: require('./images/floor.png'),
+    bird: require('./images/bird.png'),
+    pipeCore: require('./images/pipe_core.png'),
+    // pipeCore: require('./images/pipe_v2.png'),
+    // pipeTop: require('./images/pipe_top.png'),
+    pipeTop: require('./images/pipe_top_v2.png'),
+    pipeBottom: require('./images/pipe_bottom.png'),
+
+  bonuses: [
+        { uri: require('./images/CO2_.png'), width: 127, height: 103 }, 
+        { uri: require('./images/NO2_.png'), width: 127, height: 103 }, 
+        { uri: require('./images/SO2_.png'), width: 127, height: 103 }, 
+        { uri: require('./images/toxic_air.png'), width: 162, height: 519 },
+        { uri: require('./images/fire.png'), width: 218, height: 353 },
+        { uri: require('./images/fire_2.png'),width: 218, height: 353 },
+        // { uri: require('./images/toxic_air2.png'), width: 140, height: 75 },
+    ],
+}
